3d8c6ee582350a58fc49fb9984b6cf2d161f9f88
Andreas Jansson <andreasj@spotify.com>
2016-12-29 12:51:57 -0500

    how-to page

--- snoggle/server.py
+++ snoggle/server.py
@@ -386,6 +386,11 @@ def quit_game(game, player):
     return redirect(url_for('index'))
 
 
+@app.route('/howto')
+def howto():
+    return render_template('howto.html')
+
+
 @app.route('/<path:path>')
 def static_proxy(path):
     return app.send_static_file(path)
